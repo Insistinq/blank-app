847a4303a434b5fa8eff0b99b8a97bdabf931f2d
Insistinq <145484807+Insistinq@users.noreply.github.com>
2025-08-09 17:48:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new appppppp")
+st.title("TESTING 1 2 3")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
